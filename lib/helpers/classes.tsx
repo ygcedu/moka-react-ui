cd5412d9105cb5fe5d0de6aaaffc6831f80dc273
ygcedu <937601471@qq.com>
2021-10-28 00:08:43 +0800

    使用 Boolean 替代 v => v

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -1,8 +1,6 @@
-const isTrusy = (value: any) => value;
-
 function classes(...names: (string | undefined)[]) {
   // v => v 是什么就是什么（常用）
-  return names.filter(isTrusy).join(' ');
+  return names.filter(Boolean).join(' ');
 }
 
 export default classes;

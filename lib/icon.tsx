48f74f218d6197c3c02df2f4fc43a424bc0ca04b
ygcedu <937601471@qq.com>
2021-10-27 22:47:42 +0800

    添加 onClick 事件（函数类型断言）

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -4,11 +4,13 @@ import './icon.scss';
 
 interface IconProps {
   name: string;
+  // onClick: React.MouseEventHandler<SVGElement>
+  onClick: (e: React.MouseEvent) => void
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <svg className="mk-icon">
+    <svg className="mk-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`}/>
     </svg>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,9 +2,15 @@ import * as React from 'react';
 import ReactDom from 'react-dom';
 import Icon from './icon';
 
+const fn = (e: React.MouseEvent<SVGElement | SVGUseElement>) => {
+  console.log(e.currentTarget);
+  // 强行断言（武断），消除 ts 警告
+  console.log((e.target as SVGUseElement).href);
+};
+
 ReactDom.render(
   <div>
-    <Icon name="wechat"/>
-    <Icon name="alipay"/>
-    <Icon name="qq"/>
+    <Icon name="wechat" onClick={fn}/>
+    <Icon name="alipay" onClick={fn}/>
+    <Icon name="qq" onClick={fn}/>
   </div>, document.querySelector('#root'));

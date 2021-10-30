a6661eb261b832fc80ed018e64c9407755589433
ygcedu <937601471@qq.com>
2021-10-30 15:30:03 +0800

    dialog 组件基本功能具备

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import './dialog.scss';
+import {Icon} from '../index';
+import {scopedClassMaker} from '../classes';
 
 interface Props {
   visible: boolean
 }
 
+const scopedClass = scopedClassMaker('mk-dialog');
+const sc = scopedClass;
+
 const Dialog: React.FunctionComponent<Props> = (props) => {
   return (
-    props.visible ? <div> Dialog </div> : null
+    // props.children 可能不是组件
+    props.visible ?
+      <Fragment>
+        <div className={sc('mask')}>
+        </div>
+        <div className={sc()}>
+          <div className={sc('close')}>
+            <Icon name="close"/>
+          </div>
+          <header className={sc('header')}>提示</header>
+          <main className={sc('main')}>
+            {props.children}
+          </main>
+          <footer className={sc('footer')}>
+            <button>ok</button>
+            <button>cancel</button>
+          </footer>
+        </div>
+      </Fragment>
+      : null
   );
 };
 
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,3 @@
-import * as React from 'react';
-import ReactDom from 'react-dom';
-import Icon from './icon/icon';
+export {default as Icon} from './icon/icon';
 
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target);
-};
-
-ReactDom.render(
-  <div>
-    <Icon name="wechat" onClick={fn}/>
-    <Icon name="alipay"
-          onMouseEnter={() => console.log('enter')}
-          onMouseLeave={() => console.log('leave')}/>
-    <Icon name="qq" onClick={fn}/>
-  </div>, document.querySelector('#root'));
+import './index.scss';

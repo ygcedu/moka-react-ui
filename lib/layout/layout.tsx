f81c0c917c1f535f99d4f0736f1daa0a9680d69e
ygcedu <937601471@qq.com>
2021-10-31 01:09:51 +0800

    对 Aside 组件特殊处理

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import {scopedClassMaker} from '../classes';
 import './layout.scss';
+import Aside from './aside';
 
 const sc = scopedClassMaker('mk-layout');
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
+  children: ReactElement | Array<ReactElement>
 }
 
 const Layout: React.FunctionComponent<Props> = (props) => {
   const {className, ...rest} = props;
+  let hasAside = false;
+  if ((props.children as Array<ReactElement>).length) {
+    (props.children as Array<ReactElement>).map(node => {
+      if (node.type === Aside) {
+        hasAside = true;
+      }
+    });
+  }
+
   return (
-    <div className={sc('', {extra: className})} {...rest}>
+    <div className={sc('', {extra: [className, hasAside && 'hasAside'].join(' ')})} {...rest}>
       {props.children}
     </div>
   );
